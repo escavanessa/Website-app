23bec0288d26e6ca68043830251314989d8a49ce
vanessa <escavanessa@gmail.com>
2023-10-24 15:01:52 -0400

    added single form element w a small test

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,6 +1,29 @@
+import { useState } from "react"
+
+//the form element takes a value attribute with the getter and then takes the onchange attribute
+//which takes the setter function (handlesubmit) this defines some action for the onchange. inside the
+//handlesubmit, take in the event and update the state(setter) with the event target value
+
 const Contact = () => {
+    const [input, setInput] = useState('')
+    
+    const handleSubmit = (e) => {
+//prevent default
+        setInput(e.target.value)
+    }
+
+
     return(
-        <h1>Contact Me!</h1>
+        <form>
+            <label> Name:
+                <input 
+                type='text'
+                value={input}
+                onChange={handleSubmit}
+                />
+            </label>
+            <p>input value: {input}</p>
+        </form>
     )
 }
 
