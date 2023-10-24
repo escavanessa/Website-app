a89d0cc43d643d467a80e60f3ef3d8f8819e1f61
vanessa <escavanessa@gmail.com>
2023-10-24 16:33:16 -0400

    finally styled the form in a reasonable way ty ty

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -4,26 +4,72 @@ import { useState } from "react"
 //which takes the setter function (handlesubmit) this defines some action for the onchange. inside the
 //handlesubmit, take in the event and update the state(setter) with the event target value
 
+
+//when you have multiple input fields, you can add 'props set to empty string to the useState (how ever many
+//you need) then you can go into each input field and in the value attribute add the getter name dot the 
+//corresponding prop in the usestate argument.
+
+//the form element takes the onSubmit attribute which is equal to the handleSubmit function. the actual 
+//individual inputs onChange will be set to the handleChange function. it extracts the name and value
+//of the form field and then updates the input state variable using the setinput function
+
 const Contact = () => {
-    const [input, setInput] = useState('')
+    const [input, setInput] = useState({name:'', email:'', message:''});
+
+
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setInput((prevInput) => ({...prevInput, [name]: value}));
+    }
+
+    const clearForm = () => {
+        setInput("");
+    }
+
     
     const handleSubmit = (e) => {
-//prevent default
-        setInput(e.target.value)
+        e.preventDefault();
     }
 
 
     return(
-        <form>
-            <label> Name:
-                <input 
-                type='text'
-                value={input}
-                onChange={handleSubmit}
-                />
-            </label>
-            <p>input value: {input}</p>
-        </form>
+            <form className="form-container" onSubmit={handleSubmit}>
+                <div className="form-input">
+                    <label>
+                        <input 
+                        type='text'
+                        id="name"
+                        name="name"
+                        value={input.name}
+                        onChange={handleChange}
+                        placeholder="Name"
+                        />
+                    </label>
+                    <label>
+                        <input 
+                        type=""
+                        id="email"
+                        name="email"
+                        value={input.email}
+                        onChange={handleChange}
+                        placeholder="email"
+                        />
+                    </label>
+                </div>
+                <div className="form-text">
+                    <label>
+                        <textarea 
+                        id="message"
+                        name="message"
+                        value={input.message}
+                        onChange={handleChange}
+                        />
+                    </label>
+
+                    <button className="form-btn" onClick={() => clearForm}  type="submit">Submit</button>
+                </div>
+            </form>
     )
 }
 
