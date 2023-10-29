bfdda02495c18da89936039cc43f6993ec1dbcc5
galford1 <galford@tillster.com>
2023-10-29 03:24:18 -0700

    Add input form field validation

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -27,9 +27,11 @@ const images = [
         title: 'Lorem Ipsum'
     },
   ]
-  
-const map = images.map((img) => {
-    return <ProjectCard imgUrl={img.imgUrl} title={img.title}/>
+
+// the second argument in the map function is the index. kind of like i in a for loop
+// since each index is unique can be used as a unique key
+const map = images.map((img, imagesArrayIndex) => {
+    return <ProjectCard imgUrl={img.imgUrl} title={img.title} key={imagesArrayIndex}/>
 })
 
 
